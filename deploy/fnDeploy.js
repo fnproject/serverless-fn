7e6fac5971bf92d8660d2e3ccf696d61e0edd37e
James <james.jeffrey@oracle.com>
2018-03-22 16:23:55 -0700

    MVP!!!! ✨

--- deploy/fnDeploy.js
+++ deploy/fnDeploy.js
@@ -14,89 +14,115 @@ class FNDeploy {
         this.provider = this.serverless.getProvider('fn');
 
         this.hooks = {
-            'deploy:deploy': () => BB.bind(this).then(this.deploy),
+            'deploy:deploy': () => BB.bind(this).then(this.prepareFuncs)
+            .mapSeries(this.deployFunc),
         };
     }
 
-    deploy() {
-        return new BB((resolve, reject) => {
-                /*
-                    load app file  determine app name but also config --app
-                    flag... Not needed since need service.yaml
-                    deploy all functions
+    prepareFuncs() {
+        /*
+            load app file  determine app name but also config --app
+            flag... Not needed since need service.yaml
+            deploy all functions
 
-                    registry... inyaml
-                    app file in yaml
-                    appname in yaml
-                    Here always deploys all funcs in yaml
-                    Use same deploy func for single as multi and iterate over
+            registry... inyaml
+            app file in yaml
+            appname in yaml
+            Here always deploys all funcs in yaml
+            Use same deploy func for single as multi and iterate over
 
-                    deploy func:
-                    build docker image // exec to docker... Lang
-                    helpers etc... Grrr Start with docker file only.. Then move on from there....
-                    docker push // exec to docker
-                    update route in fn. // json post to fn using fn api json
-                */
-                // var dockerUser = this.serverless.service.provider['fn-user'];
+            deploy func:
+            build docker image // exec to docker... Lang
+            helpers etc... Grrr Start with docker file only.. Then move on from there....
+            docker push // exec to docker
+            update route in fn. // json post to fn using fn api json
+        */
+        // var dockerUser = this.serverless.service.provider['fn-user'];
 
-            const appName = this.serverless.service.serviceObject.name;
-            if (appName === '' || appName === undefined) {
-                reject('No service name defined in serverless yaml.');
-                return;
-            }
-            const svc = this.serverless.service.serviceObject;
-                // console.log(appName)
-                // console.log(svc)
-                // bump version // increase number seems easy enough...
-            const cwd = process.cwd();
 
-            let allGood = true;
-            let funcFound = false;
-            _.each(this.serverless.service.functions, (func, dir) => {
-                try {
-                        // Each function should have dir. First change to it.
-                    if (!fs.existsSync(dir)) {
-                        reject(`function ${dir}, does not exist`);
-                        return;
-                    }
+        const appName = this.serverless.service.serviceObject.name;
+        if (appName === '' || appName === undefined) {
+            BB.reject('No service name defined in serverless yaml.');
+        }
+        const svc = this.serverless.service.serviceObject;
+            // console.log(appName)
+            // console.log(svc)
+            // bump version // increase number seems easy enough...
+        const cwd = process.cwd();
 
-                    if (dir === cwd) {
-                        // Root func. Cant happen right now. Need special logic.
-                    } else {
-                        process.chdir(dir);
-
-                        if (!(func.name != '')) {
-                            func.name = dir;
-                        }
-
-                        if (func.path === '' | func.path === undefined || func.path === null) {
-                            func.path = dir;
-                        }
-                    }
-
-                    this.mergeConfigs(svc, func);
-
-                    // Maybe start convo about weather we should keep the docker
-                    // file there or not. Since the lang helpers can change.
-                    this.deployFunc(func, appName, cwd, `${cwd}/${dir}`, reject);
-
-                    funcFound = true;
-                } catch (err) {
-                    allGood = false;
-                    reject(err);
-                }
-            });
-            if (!allGood) {
-                reject('Should have left in the reject on error.');
-                return;
-            }
-
-            if (!funcFound) {
-                reject('No functions found to deploy');
-                return;
-            }
-            resolve('Got to the end');
+        const funcs = [];
+        _.each(this.serverless.service.functions, (func, dir) => {
+            func.appName = appName;
+            funcs.push({ func, dir, cwd, svc });
         });
+        return funcs;
+    }
+
+
+    deployFunc(funcDir) {
+        const { func,
+             dir,
+             cwd,
+             svc } = funcDir;
+         // XXX local flag
+         // XXX noCache flag
+
+        const steps = [
+            this.bumpIt,
+            this.localCmd,
+            this.dockerBuild,
+            this.dockerPush,
+            this.routeUpdate,
+        ];
+
+        // try {
+            // Each function should have dir. First change to it.
+        if (!fs.existsSync(dir)) {
+            return BB.reject(`function ${dir}, does not exist`);
+        }
+
+        this.mergeConfigs(svc, func);
+
+        if (dir !== cwd) {
+            process.chdir(dir);
+
+            func.dir = `${cwd}/${dir}`;
+
+            if (!(func.name != '')) {
+                func.name = dir;
+            }
+
+            if (func.path === '' | func.path === undefined || func.path === null) {
+                func.path = dir;
+            }
+        }
+
+        if (!fs.existsSync('Dockerfile')) {
+            // XXX logic for no Dockerfile in the func folder.
+            // XXX add the pre build and after build to steps list if there is nod docker file.
+            // Lang helper stuff
+            // if (false && (helper !== undefined && helper !== null)) {
+                // post lang helper.
+                // return db.then(() => { helper.afterBuild; });
+            // }
+            return BB.reject(`cannot find Dockerfile: ${func.name}`);
+        }
+
+        // Root func. Cant happen right now. Need special logic.
+        // Figure out this...
+        // return this.deployFuncWrapper(func, appName, cwd, `${cwd}/${dir}`);
+
+
+        return BB.mapSeries(steps, (s) => {
+            s(func);
+        });
+
+        // Maybe start convo about weather we should keep the docker
+        // file there or not. Since the lang helpers can change.
+
+        // } catch (err) {
+        //     fail(err);
+        // }
     }
 
     mergeConfigs(svc, func) {
@@ -112,32 +138,6 @@ class FNDeploy {
         }
     }
 
-    deployFunc(func, appName, baseDir, fdir, reject) {
-        // XXX Get this from args... Figure out how to pass args
-        const noCache = false;
-        this.bumpIt(func);
-
-        this.buildFunc(func, fdir, noCache, reject);
-
-        // XXX add flag for local.. Figure out flags
-        if (true) { // local flag
-            this.dockerPush(func, reject);
-        }
-
-        // var apiInstance = new FN.AppsApi();
-        // apiInstance.apiClient.basePath = 'http://127.0.0.1:8080/v1'.replace(/\/+$/, '');
-        //
-        // var app = "serverless-hello-world"; // String | name of the app.
-        //
-        // apiInstance.appsAppGet(app).then(function(data) {
-        //     console.log('API called successfully. Returned data: ' + data);
-        //     resolve(data)
-        // }, function(error) {
-        //     // console.error(error.response.text);
-        //     reject(error.response.text)
-        // });
-    }
-
     bumpIt(func) {
         // Should we just bump. Verify that this is a valid version...?
         // Or should we check to see currently deployed version. And use that and call bump on that?
@@ -150,68 +150,64 @@ class FNDeploy {
         // Add the rest of the logic to make sure version is not blank etc... Save.
     }
 
-    buildFunc(func, fdir, noCache, reject) {
-        this.localBuild(fdir, func.build, reject);
-
-        this.dockerBuild(fdir, func, noCache, reject);
-    }
-
-    localBuild(fdir, buildCmds, reject) {
-        console.log('Localbuild');
-        console.log(fdir, buildCmds);
-        if (buildCmds !== undefined && buildCmds !== null) {
-            _.each(buildCmds, (cmd) => {
-                console.log(cmd);
-                const res = spawnSync('/bin/sh', ['-c', cmd], { stdio: 'inherit', cwd: fdir });
-                if (res.status > 0) {
-                    reject(`${cmd} failed: ${res.status}`);
-                }
-            });
-        }
-    }
-
-    dockerBuild(fdir, func, noCache, reject) {
-        // Check docker version
-        if (!fs.existsSync(`${fdir}/Dockerfile`)) {
-            reject(new Error(`cannot find ${fdir}/Dockerfile`));
-            // Lang helper stuff
-            return;
+    localCmd(func) {
+        if (func.build === undefined || func.build === null) {
+            return BB.resolve('im done 😂');
         }
 
-        this.runDockerBuild(fdir, func, noCache, reject);
-
-        if (false && (helper !== undefined && helper !== null)) {
-            // post lang helper.
-            const err = helper.afterBuild();
-            if (err !== undefined) {
-                reject(err);
+        return BB.each(func.build, (cmd) => {
+            const { dir } = func;
+            console.log(cmd);
+            const res = spawnSync('/bin/sh', ['-c', cmd], { stdio: 'inherit', cwd: dir });
+            if (res.status > 0) {
+                return BB.reject(`${cmd} failed: ${res.status}`);
             }
-        }
+            return BB.resolve('command succeeded');
+        });
     }
 
-    runDockerBuild(fdir, func, noCache, reject) {
-        const args = ['build', '-t', func.imageName, '-f', `${fdir}/Dockerfile`];
-        if (noCache) {
+    dockerBuild(func) {
+        const args = ['build', '-t', func.imageName, '-f', `${func.dir}/Dockerfile`];
+        if (func.noCache) {
             args.push('--no-cache');
         }
         args.push('--build-arg', 'HTTP_PROXY');
         args.push('--build-arg', 'HTTPS_PROXY');
-        args.push(fdir);
+        args.push(func.dir);
         console.log(args);
         const res = spawnSync('docker', args, { stdio: 'inherit' });
         if (res.status !== 0) {
             console.log(res);
-            reject(new Error('docker command failed'));
+            return BB.reject('docker command failed');
         }
+        return BB.resolve('image built successfully');
     }
 
-    dockerPush(func, reject) {
+    dockerPush(func) {
+        if (func.local) { // local flag
+            return BB.resolve('local do not docker push');
+        }
         // XXX check valid image name..
-        this.serverless.log(`Pushing ${func.imageName} to docker registry....`);
+        console.log(`Pushing ${func.imageName} to docker registry....`);
         const res = spawnSync('docker', ['push', func.imageName], { stdio: 'inherit' });
         if (res.status !== 0) {
-            reject(new Error(`failed to push ${func.imageName}`));
+            return BB.reject(`failed to push ${func.imageName}`);
         }
+        return BB.resolve('func pushed');
+    }
+
+    routeUpdate(func) {
+        const { appName } = func;
+        const apiInstance = new FN.RoutesApi();
+        apiInstance.apiClient.basePath = 'http://127.0.0.1:8080/v1'.replace(/\/+$/, '');
+
+        const body = new FN.RouteWrapper(); // RouteWrapper | One route to post.
+        body.route = {
+            image: func.imageName,
+            config: func.config,
+        };
+
+        return apiInstance.appsAppRoutesRoutePut(appName, func.path, body);
     }
 }
 
