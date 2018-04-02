98f6723b62677c760a29ac95438a84eb2d5ed3e4
James <james.jeffrey@oracle.com>
2018-04-02 12:04:50 -0700

    Opened invoke. Just lint

--- invoke/fnInvoke.js
+++ invoke/fnInvoke.js
@@ -1,20 +1,20 @@
 'use strict';
 
 class FNInvoke {
-  constructor(serverless, options) {
-    this.serverless = serverless;
-    this.options = options || {};
-    this.provider = this.serverless.getProvider('fn');
+    constructor(serverless, options) {
+        this.serverless = serverless;
+        this.options = options || {};
+        this.provider = this.serverless.getProvider('fn');
 
-    this.hooks = {
-      'invoke:invoke': this.invokeFunction.bind(this),
-    };
-  }
+        this.hooks = {
+            'invoke:invoke': this.invokeFunction.bind(this),
+        };
+    }
 
-  invokeFunction(func, data) {
-      console.log(func, data)
-      this.serverless.cli.log("I am invoking a functions")
-  }
+    invokeFunction(func, data) {
+        console.log(func, data);
+        this.serverless.cli.log('I am invoking a functions');
+    }
 }
 
 module.exports = FNInvoke;
