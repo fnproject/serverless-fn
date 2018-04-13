5f089e2d8abec41cc1cbc7ff96219c5f0ff8c180
James <james.jeffrey@oracle.com>
2018-04-13 09:23:27 -0700

    Add template for node service

---
+++ templates/node/hello/func.js
@@ -0,0 +1,10 @@
+var fdk=require('@fnproject/fdk');
+
+fdk.handle(function(input){
+  var name = 'World';
+  if (input.name) {
+    name = input.name;
+  }
+  response = {'message': 'Hello ' + name}
+  return response
+})
